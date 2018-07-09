9222a4791703311adbe44db12d75090d3ee054e1
Win Giamportone <wgiamportone@hugeinc.com>
2018-07-09 17:21:34 -0400

    Adding app and component markup along with component styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './scss/app.scss';
+import Accordion from './components/accordion';
 
 class App extends Component {
   render() {
     return (
-      <div className="">
-
+      <div className="grid">
+        <Accordion className="grid-half" title="Section 1">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisques commodo purus quis mi cursus hendrerit eu eu metus. Aliquam aliquam arcu eget aliquet scelerisque. Pellentesque sodales turpis vitae venenatis vehicula.</p>
+          <p>Ut id porta velit. Ut eu dignissim dui, quis gravida est. Cras quis venenatis mauris, a bibendum enim. Sed at augue libero. Nullam tor tor metus, tincidunt ut urna id, posuere placerat orci. Ut quis risus dictum risus facilisis imperdiet quis sed eros.</p>
+        </Accordion>
       </div>
     );
   }
--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import './style.scss';
 
 class Accordion extends Component {
-
+  render() {
+    return (
+      <div className={"accordion " + this.props.className}>
+        <button className="accordion__link">
+          <span className="accordion__title">{this.props.title}</span>
+        </button>
+        <div className="accordion__panel">
+          <div className="accordion__panel_inner" ref="inner">
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Accordion;
